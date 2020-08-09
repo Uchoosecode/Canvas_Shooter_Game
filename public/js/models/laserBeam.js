717d4bade3fb681b3defaaacbfad7e428839ff90
Uchoosecode <awittyl44@gmail.com>
2020-08-09 12:02:42 -0400

    starting position, draw andvstatic drawAll

--- public/js/models/laserBeam.js
+++ public/js/models/laserBeam.js
@@ -2,8 +2,26 @@ class LaserBeam {
     static all = [];
 
     constructor(position) {
+        this.startingPosition(position);
         this.color = 'yellow';
-        this.position = position;
         LaserBeam.all.push(this);
+    }
+
+    startingPosition() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.position.x, this.position.y, this.width = 4, this.height = 10)
+    }
+
+    update() {
+
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+
+    static drawAll() {
+        LaserBeam.all.forEach(laserbeam => this.draw());
     }
 }
